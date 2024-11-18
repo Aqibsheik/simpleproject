0e3419a1f5fab3abfb155a814714f48cf1dab668
Aqibsheik <aqibsheik5@gmail.com>
2024-11-18 09:52:21 +0530

    pattern

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ else{
     console.log("odd number")
 }
 
+//pattern printing
 
+num=4;
+for(let i=1;i<=num;i++)
+{
+    let bag=""
+    for(let j=1;j<=num;j++)
+    {
+        bag=bag+"*"
+    }
+    console.log(bag)
+}
